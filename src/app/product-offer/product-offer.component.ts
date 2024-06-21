b597dd8f718e5572425fa7caabc0a74f085cd3b3
Terence Faid JABO <faidterence@outlook.com>
2024-06-21 10:31:05 +0200

    feat(routing):configure path routing and respective components

---
+++ src/app/product-offer/product-offer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-offer',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-offer.component.html',
+  styleUrl: './product-offer.component.css'
+})
+export class ProductOfferComponent {
+
+}

b06eadd7cfcbc49646fd9ed4e3f9d780167939a3
Terence Faid JABO <faidterence@outlook.com>
2024-06-21 10:53:22 +0200

    feat: add HttpClientModule to application configuration
    - Import HttpClientModule in app.config.ts
    - Ensure HttpClientModule is included in the providers array using importProvidersFrom
    - Resolve NullInjectorError for HttpClient in ProductsService

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
-export class ProductComponent implements OnInit{
-
+export class ProductComponent implements OnInit {
   productDetails?: any;
 
-  constructor(private route: ActivatedRoute, private prod) { }
+  constructor(
+    private route: ActivatedRoute,
+    private productSerivce: ProductsService
+  ) {}
 
+  ngOnInit(): void {
+    let productId = this.route.snapshot.params['id'];
+    this.productSerivce.getProductDetailById(productId).subscribe((data) => {
+      this.productDetails = data;
+    });
+  }
 }
